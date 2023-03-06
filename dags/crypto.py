ccc609e71a805cc71683a70549db1f51c7dbdf87
Victor <victor.santos.cd@gmail.com>
2023-03-06 13:38:08 -0300

    feat(dags.crypto): Create the create_table task using PostgresOperator

--- dags/crypto.py
+++ dags/crypto.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from datetime import datetime
 from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 from tasks.api_tasks import is_api_available
 
 
@@ -12,4 +13,16 @@ with DAG(dag_id="crypto", start_date=datetime(2023, 3, 1),
         python_callable=is_api_available
     )
 
-    is_api_available
+    create_table = PostgresOperator(
+        task_id="create_table",
+        postgres_conn_id="postgres",
+        sql="""
+            CREATE TABLE IF NOT EXISTS cripto (
+                crypto_symbol TEXT NOT NULL,
+                crypto_price TEXT NOT NULL,
+                record_date TEXT NOT NULL
+            )
+        """
+    )
+
+    #create_table >> is_api_available
